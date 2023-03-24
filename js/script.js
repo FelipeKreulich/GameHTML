b0f55af775d9ee7e8e939c208eb300692a769797
FelipeKreulich <felipek2002k@gmail.com>
2023-03-24 19:53:27 -0400

    V1.1

--- js/script.js
+++ js/script.js
@@ -1,54 +1,71 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const clouds = document.querySelector('.clouds');
-const restartBtn = document.querySelector('.restart');
-let gameOver = false; // Nova variável gameOver
+mario = document.querySelector('.mario');
+pipe = document.querySelector('.pipe');
+clouds = document.querySelector('.clouds');
+textStart = document.querySelector('.textStart');
+points = document.querySelector('#contador');
 
-const jump = () => {
-  if (!gameOver) { // Verifica se gameOver é false
-    mario.classList.add('jump');
-    mario.src = './images/jumpmario.png';
-    mario.style.width ='85px'; 
-    setTimeout(()=> {
-      mario.classList.remove('jump');
-      if (!gameOver) { // Verifica se gameOver é false
-        mario.src = './images/mario.gif';
-        mario.style.width = '150px';
-      }
-    }, 500);
-  }
+let contador = 0;
+
+// Função Start do Game
+const start = () => {
+
+  textStart.style.color = "rgb(236,236,236)";
+
+  pipe.classList.add('pipe-animation');
+  mario.style.width = "150px";
+  mario.style.marginLeft = "50px";
+  mario.src = "./images/mario.gif";
+  clouds.classList.add('clouds-animation-movement');
+
 }
 
-const loop = setInterval(() => {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+document.addEventListener('click', start);
 
-  if(pipePosition <= 140 && pipePosition> 0 && marioPosition < 80) {
+// Função Pulo
+const jump = () => {
+  mario.classList.add('jump');
+  mario.src = "./images/jumpmario.png"
+  mario.style.width = "85px";
+
+  setTimeout(() => {
+    mario.classList.remove('jump');
+    mario.src = "./images/mario.gif";
+    mario.style.width = "150px";
+  }, 1500);
+}
+
+document.addEventListener('keydown', jump);
+
+// Função para atualizar o score
+const updateScore = () => {
+  contador++
+  points.innerText = contador;
+}
+
+// Função GameOver
+
+const checkGameOver = setInterval(() => {
+
+  const pipePostion = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', "");
+
+  if (pipePostion <= 120 && pipePostion > 0 && marioPosition < 80) {
+
     pipe.style.animation = 'none';
-    pipe.style.left = `${pipePosition}px`;
+    pipe.style.left = `${pipePostion}px`;
 
     mario.style.animation = 'none';
     mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = './images/gameover.png';
-    mario.style.width = '120px';
-    mario.style.marginLeft = '20px';
+    mario.src = "./images/gameover.png";
+    mario.style.width = "120px";
+    mario.style.marginLeft = "5px";
 
-    clouds.style.animationPlayState = 'paused'; // Pausa a animação das nuvens
-    
-    gameOver = true; // Define gameOver como true
-
-    restartBtn.style.display = "block";
-    restartBtn.addEventListener('click', restartGame)
-
-    clearInterval(loop);
+    clouds.style.animationPlayState = 'paused';
   }
-}, 10)
 
-const restartGame = () => {
-  restartBtn.style.display = "none";
-  gameOver = false;
-}
+  if (pipePostion <= 60 && pipePostion > 0 && marioPosition >= 80) {
+    updateScore();
+  }
 
-document.addEventListener('click', jump);
-document.addEventListener('keydown', jump);
+}, 10);
